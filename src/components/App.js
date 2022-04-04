5f9a27c1867c18ec7324a08c02705b9640f7755c
Deepak Pal <deepak.pal@technia.com>
2022-04-04 20:57:02 +0530

    Header Compnent

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+const App = () => {
+    return (
+        <div>
+          <Header></Header>
+        </div>
+       );
+}
+export default App;
